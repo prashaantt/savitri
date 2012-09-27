2f2daa013d1c286113f0cd411839cebde569dad8
Nishant Modak <modak.nishant@gmail.com>
2012-09-27 13:24:13 +0530

    tags-acts_as_taggable_on

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120926182141) do
+ActiveRecord::Schema.define(:version => 20120927071105) do
 
   create_table "cantos", :force => true do |t|
     t.integer  "cantono"
@@ -53,13 +53,21 @@ ActiveRecord::Schema.define(:version => 20120926182141) do
     t.integer  "canto_id"
   end
 
-  create_table "tags", :force => true do |t|
-    t.string   "name"
-    t.integer  "post_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       :limit => 128
+    t.datetime "created_at"
   end
 
-  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
 end
