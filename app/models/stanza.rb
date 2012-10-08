ce72706cdd1874ff25e6bbd7f5e0bda0d63a9931
NM <nm@nm-desktop.(none)>
2012-10-08 17:05:31 +0530

    fixes ordering for lines via stanza #2

--- app/models/stanza.rb
+++ app/models/stanza.rb
@@ -1,6 +1,6 @@
 class Stanza < ActiveRecord::Base
   attr_accessible :stanzno, :canto_id
-  has_many :lines
+  has_many :lines, :order => 'no'
   belongs_to :canto
 
   validates :stanzno , :uniqueness => true
