e49cd6cf9a6124bf1ca8ab3fa2c80e312305728c
NM <nm@nm-desktop.(none)>
2012-10-08 14:25:23 +0530

    ajaxify test

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -3,5 +3,11 @@ class ReadController < ApplicationController
   	#@cantos = Canto.includes("stanzas").includes("lines").order("cantono").page(params[:cantos]).per(1)
   	#@lines = Line.order("no").page(params[:cantos]).per(5)
   	@stanzas = Stanza.includes("lines").order("stanzno").page(params[:stanzas]).per(4)
+
+  	respond_to do |format|
+      format.html # index.html.erb
+      format.js 
+      format.json { render json: @stanzas }
+    end
   end
 end
