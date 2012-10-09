cf6aaaec4f968f16a2596125196f7b5a6b36aa1e
NM <nm@nm-desktop.(none)>
2012-10-09 15:56:56 +0530

    read/<cantoNo> and tree left nav testing #5

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -10,4 +10,18 @@ class ReadController < ApplicationController
       format.json { render json: @stanzas }
     end
   end
+
+  def show
+    
+    @canto =  Canto.find(params[:id])
+    @stanzas = @canto.stanzas.includes("lines").order("stanzno").page(params[:stanzas]).per(4)
+    #@stanzas = Stanza.find(:conditions => { :canto_id => @canto.id}) 
+#    @stanzas = Stanza.includes("lines").order("stanzno").page(params[:stanzas]).per(4)
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js
+      format.json { render json: @stanzas }
+    end
+  end
 end
