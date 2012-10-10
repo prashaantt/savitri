85230858da0302634dea044bca919c53903a2036
NM <nm@nm-desktop.(none)>
2012-10-10 18:33:28 +0530

    cantos related to book, new canto-book reference

--- app/models/canto.rb
+++ app/models/canto.rb
@@ -1,8 +1,9 @@
 class Canto < ActiveRecord::Base
-  attr_accessible :cantono, :description, :title
+  attr_accessible :cantono, :description, :title, :book_id
   has_many :stanzas
   has_many :lines , :through => :stanzas
-
+  belongs_to :books
+  
   validates :cantono, :title, :uniqueness => true
   validates :cantono, :title, :presence=> true
   
