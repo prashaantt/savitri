31002d29f6d5424e01d63a6d609321ea5ae69ef3
NM <nm@nm-desktop.(none)>
2012-10-12 16:23:18 +0530

    email confirmations, fix #8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
  
   def store_location
-    session[:resource_return_to] = request.fullpath
+      session[:resource_return_to] = request.fullpath
   end
 
   def after_sign_in_path_for(resource)
-    session[:resource_return_to]
+    session[:resource_return_to] || super
   end
 
   def after_sign_out_path_for(resource_or_scope)
