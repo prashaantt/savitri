29201dd334d7db4aab240d0b5eee5dc7b545d9ab
NM <nm@nm-desktop.(none)>
2012-12-19 16:19:46 +0530

    #21 default image for profile

--- app/uploaders/user_photo_uploader.rb
+++ app/uploaders/user_photo_uploader.rb
@@ -20,6 +20,10 @@ class UserPhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+   def default_url
+    [version_name, "rails.png"].compact.join('_')
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
