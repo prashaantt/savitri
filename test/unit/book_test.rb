204383bd26433a2371a9a85955b76074aaab79b4
pratik <pratik@pratik-dell.(none)>
2012-12-27 16:07:02 +0530

    Test related changes for book and lines models.

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,7 +1,65 @@
-require 'test_helper'
+#require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "book is not valid without a unique name" do
+    book = Book.new(:name => books(:one).name,
+      :description => "yyy",
+      :no => 3)
+
+    assert !book.save
+    assert_equal "has already been taken", book.errors[:name].join('; ')
+  end
+
+
+  test "book is not valid without a unique no" do
+    book = Book.new(:name => "new book",
+      :description => "yyy - only say hello",
+      :no => books(:one).no)
+
+    assert !book.save
+    assert_equal "has already been taken", book.errors[:no].join('; ')
+  end
+
+
+  test "book is not valid without name" do
+    book = Book.new(:description => "yyy",
+      :no => 4)
+
+    assert !book.save
+    assert_equal "can't be blank", book.errors[:name].join('; ')
+  end
+
+
+  test "book is not valid without no" do
+    book = Book.new(:name => "new book",
+      :description => "yyy")
+
+    assert !book.save
+    assert_equal "can't be blank", book.errors[:no].join('; ')
+  end
+
+  test "perform crud on book" do
+      
+      book = Book.new(:name => "new book", 
+        :description =>"This is description for a good book to be saved",
+        :no => 7)
+
+      #1. create
+      assert book.save, "can't be saved"
+      
+      #4. read
+      book_new = Book.find(book.id)
+      assert_equal book.name, book_new.name
+
+      #2. update
+      book.name = "Sherlok holmes"
+      assert book.save, "can't update the book info"
+
+      #3. delete
+      assert book.destroy, "can't destroy"
+  end
 end
--- test/unit/line_test.rb
+++ test/unit/line_test.rb
@@ -1,7 +1,63 @@
-require 'test_helper'
+#require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class LineTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "line is not valid without a unique no" do
+    line = Line.new(:no => 1,
+      :line => "yyy - only say hello",
+      :stanza_id => 1)
+
+    assert !line.save
+    assert_equal "has already been taken", line.errors[:no].join('; ')
+  end
+
+
+  test "line is not valid without stanza_id" do
+    line = Line.new(:line => "yyy",
+      :no => 4)
+
+    assert !line.save
+    assert_equal "can't be blank", line.errors[:stanza_id].join('; ')
+
+  end
+
+  test "line is not valid without line" do
+    line = Line.new(:no => 4,
+      :stanza_id => 1)
+    assert !line.save
+    assert_equal "can't be blank", line.errors[:line].join('; ')
+  end
+
+  test "line is not valid without no" do
+    line = Line.new(:line => "yyy",
+      :stanza_id => 1)
+    assert !line.save
+    assert_equal "can't be blank", line.errors[:no].join('; ')
+  end
+
+
+  test "perform crud on line" do
+      
+      line = Line.new(:line =>"This is line for a good line to be saved",
+        :no => 7,
+        :stanza_id=>2)
+
+      #1. create
+      assert line.save, "can't be saved"
+      
+      #4. read
+      line_new = Line.find(line.id)
+      assert_equal line.line, line_new.line
+
+      #2. update
+      line.line = "Sherlok holmes"
+      assert line.save, "can't update the line info"
+
+      #3. delete
+      assert line.destroy, "can't destroy"
+  end
 end
