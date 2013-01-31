b8a6130e3f07851a8a8c15f2b43b8951e00c35c0
NM <modak.nishant@gmail.com>
2013-01-31 17:05:33 +0530

    ransack integration 101

--- app/models/line.rb
+++ app/models/line.rb
@@ -13,4 +13,10 @@ class Line < ActiveRecord::Base
   	no
   end
 
+  UNRANSACKABLE_ATTRIBUTES = ["id", "created_at", "updated_at", "stanza_id"]
+
+  def self.ransackable_attributes auth_object = nil
+    (column_names - UNRANSACKABLE_ATTRIBUTES) + _ransackers.keys
+  end
+
 end
