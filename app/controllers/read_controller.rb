e27ba79cd5db0b96bf8e0ffdb60a05154f3b0e52
Nishant Modak <modak.nishant@gmail.com>
2013-03-22 20:43:42 +0530

    Sharing of particular Lines. Related Refactoring for /annotations (notebook). + Sundry cleanup for application layout.

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -2,7 +2,7 @@ class ReadController < ApplicationController
   def index
   	#@cantos = Canto.includes("stanzas").includes("lines").order("cantono").page(params[:cantos]).per(1)
   	#@lines = Line.order("no").page(params[:cantos]).per(5)
-  	@stanzas = Stanza.includes("lines").order("no").page(params[:stanzas]).per(4)
+  	@stanzas = Stanza.includes("lines").order("no").page(params[:pages]).per(4)
 
   	respond_to do |format|
       format.html # index.html.erb
@@ -14,7 +14,7 @@ class ReadController < ApplicationController
   def show
     
     @canto =  Canto.find(params[:id])
-    @stanzas = @canto.stanzas.includes("lines").order("no").page(params[:stanzas]).per(4)
+    @stanzas = @canto.stanzas.includes("lines").order("no").page(params[:pages]).per(4)
     #@stanzas = Stanza.find(:conditions => { :canto_id => @canto.id}) 
 #    @stanzas = Stanza.includes("lines").order("stanzno").page(params[:stanzas]).per(4)
     
