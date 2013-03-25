8e272802dfd385d17a37eb5eaa4b17f2c2db4253
Nishant Modak <modak.nishant@gmail.com>
2013-03-25 16:31:32 +0530

    Unicorn Configu Update + NewRelic + Deploy.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,8 @@
 worker_processes 3
 timeout 30
-working_directory "~/projects/savitri"
-pid "~/projects/savitri/tmp/pids/unicorn.pid"
-stderr_path "~/savitri/unicorn/err.log"
-stdout_path "~/savitri/unicorn/out.log"
-listen "~/projects/savitri/tmp/unicorn.todo.socket"
+working_directory "/home/ec2-user/savitri/current"
+pid "/home/ec2-user/savitri/current/tmp/pids/unicorn.pid"
+listen "/home/ec2-user/savitri/current/tmp/unicorn.todo.socket"
+stderr_path "/home/ec2-user/savitri/current/unicorn/err.log"
+stdout_path "/home/ec2-user/savitri/current/unicorn/out.log"
+
