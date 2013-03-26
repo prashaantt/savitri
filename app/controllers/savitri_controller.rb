38cb5c1553ba22febfd8e5cf180626671550a933
Nishant Modak <modak.nishant@gmail.com>
2013-03-26 16:10:10 +0530

    Fixes #38.  Animated Random Text on Landing Page. @prashaantt

--- app/controllers/savitri_controller.rb
+++ app/controllers/savitri_controller.rb
@@ -1,4 +1,25 @@
 class SavitriController < ApplicationController
   def index
+  	 @sentence = Stanza.order("RANDOM()").last;
+  	 #@text = @sentence.lines.to_json(:only => :line)
+  	 @text = Array.new
+  	 @sentence.lines.each do |l|
+  	 	@text << l.line
+  	 end
+# 	 format.json {render :json => @section.to_json(:include => :lines)}
+
+  	 respond_to do |format|
+      format.html # show.html.erb
+      format.json {render :json => {:text => @text, :source => Section.find(@sentence.section).no.to_s + "." + @sentence.runningno.to_s }}
+    end
   end
 end
+
+
+# text4 = "{ \"text\": 
+#			[\"There is a deeper seeing from within\",
+#			 \"And, when we have left these small purlieus of mind,\",
+# 			 \"A greater vision meets us on the heights\",
+# 			 \"In the luminous wideness of the Spirit’s gaze.\"
+#			], 
+#			\"source\": \"47.3\" }"
