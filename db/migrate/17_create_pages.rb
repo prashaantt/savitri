d8b1764d90f53eb0e1178ba131d8f8971a732388
Nishant Modak <modak.nishant@gmail.com>
2013-04-12 20:22:58 +0530

    priority and not order.

--- db/migrate/17_create_pages.rb
+++ db/migrate/17_create_pages.rb
@@ -3,7 +3,7 @@ class CreatePages < ActiveRecord::Migration
     create_table :pages do |t|
       t.string :name
       t.string :permalink
-      t.integer :order
+      t.integer :priority
       t.string :category
       t.text :content
       t.text :md_content
