98125e5cae7248f3c5dff2c0b5af0834798a9345
Nishant Modak <modak.nishant@gmail.com>
2013-05-08 17:41:17 +0530

    amazon s3 storage for images. + #21.

---
+++ config/initializers/fog.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAJRJLL5KWIITWOWWA',                        # required
+    :aws_secret_access_key  => '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',                        # required
+    :region                 => 'ap-southeast-1'                  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'savitri-in-uploads'                     # required
+  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
