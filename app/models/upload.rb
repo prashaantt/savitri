555f263ad26e1fcf44460fa85d2572c605a972b0
Nishant Modak <modak.nishant@gmail.com>
2013-05-11 00:53:20 +0530

    music direct uploads to s3.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,6 @@
 class Upload < ActiveRecord::Base
-  attr_accessible :photo
+  attr_accessible :photo, :music
   belongs_to :posts
   mount_uploader :photo, PostPhotoUploader
+  #mount_uploader :music, PostMusicUploader
 end
