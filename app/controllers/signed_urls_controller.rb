ed3de79b5e2621b356fe95748ee6424170e35552
Nishant Modak <modak.nishant@gmail.com>
2013-05-12 21:34:54 +0530

    devuploads vs uploads url

--- app/controllers/signed_urls_controller.rb
+++ app/controllers/signed_urls_controller.rb
@@ -1,10 +1,16 @@
 class SignedUrlsController < ApplicationController
   def index
+    if Rails.env.production?
+      @bucket="savitri-in-uploads"
+    else
+      @bucket="savitri-in-devuploads"
+    end
+    
     render json: {
-	policy: s3_upload_policy_document,
-	signature: s3_upload_signature,
-	key: "uploads/music/#{SecureRandom.uuid}/#{params[:doc][:title]}",
-	success_action_redirect: "/"
+    	policy: s3_upload_policy_document,
+    	signature: s3_upload_signature,
+    	key: "uploads/music/#{SecureRandom.uuid}/#{params[:doc][:title]}",
+    	success_action_redirect: "/"
     }
   end
 
@@ -15,7 +21,7 @@ class SignedUrlsController < ApplicationController
       {
         expiration: 30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
         conditions: [
-          { bucket: "savitri-in-devuploads"},
+          { bucket: @bucket},
           #{ bucket: ENV['S3_BUCKET'] },
           { acl: 'public-read' },
           ["starts-with", "$key", "uploads/music/"],
