7d328c8b67eaed196d480b79eba3538da0c3e59c
NM <modak.nishant@gmail.com>
2013-06-05 13:11:55 +0530

    media centre static pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,9 +2,11 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :name, :permalink, :md_content, :priority, :category
 
   validates_length_of :permalink, :minimum => 3
-  validates_presence_of :content, :name, :permalink, :md_content, :priority
-  validates_uniqueness_of :priority, :permalink
-  validates :priority, :numericality => {:only_integer => true}
+  validates_presence_of :content, :name, :permalink, :md_content
+  validates_uniqueness_of  :permalink
+  validates_presence_of :priority, :unless => Proc.new {|m| m.category != "Menu"}
+  validates_uniqueness_of :priority, :unless => Proc.new {|m| m.category != "Menu"}
+  validates :priority, :numericality => {:only_integer => true}, :unless => Proc.new {|m| m.category != "Menu"}
 
   def to_param
   	"#{permalink}"
