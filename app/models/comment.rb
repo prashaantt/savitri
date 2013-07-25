7a29caa5f5e48bbae67e92675a1553b25116ca03
NM <modak.nishant@gmail.com>
2013-07-25 17:11:12 +0530

    recent comments posts#index sidebar. #94.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   attr_accessible :body, :commenter, :user_id
+	after_commit :flush_cache
+
+  def self.cached_recent
+  	Rails.cache.fetch([name, "recentcomments"]) { limit(5).order('created_at desc').to_a }
+  end
+
+	def flush_cache
+	  Rails.cache.delete([self.class.name, "recentcomments"])
+	end
 end
