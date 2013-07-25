9afb639c20bac6e95677666887ee8409e931953b
NM <modak.nishant@gmail.com>
2013-07-25 21:00:03 +0530

    recent 10 comments specific to the blog

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,11 +4,11 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :commenter, :user_id
 	after_commit :flush_cache
 
-  def self.cached_recent
-  	Rails.cache.fetch([name, "recentcomments"]) { limit(5).order('created_at desc').to_a }
+  def self.cached_recent(blog)
+  	Rails.cache.fetch([name, "recentcomments"+blog.title]) { blog.comments.limit(10).order('created_at desc').to_a }
   end
 
 	def flush_cache
-	  Rails.cache.delete([self.class.name, "recentcomments"])
+	  Rails.cache.delete([self.class.name, "recentcomments"+Post.find_by_id(self.post_id).blog.title])
 	end
 end
