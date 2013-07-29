1004ec0d17ff00013b12accfa64887d634d74bf4
NM <modak.nishant@gmail.com>
2013-07-29 20:01:57 +0530

    Email Notifications for comments.

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,18 @@
+class CommentMailer < ActionMailer::Base
+  default from: "admin@savitri.in"
+
+  def comment_notification_email(reciever, sender, blog, post, comment)
+    @reciever = reciever
+    @comment = comment
+    @sender = sender
+    @blog = blog
+    @post = post 
+    @url  = "http://savitri.in"
+    if @sender.name.blank?
+      name = @sender.name 
+    else
+      name = @sender.username
+  	end
+    mail(:to => reciever.email, :subject => "#{name} wrote a comment on '#{blog.title}'")
+  end
+end
