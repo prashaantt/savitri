4baf32baf245e99de6e379de387ece357c4cc9b4
NM <modak.nishant@gmail.com>
2013-08-05 15:25:28 +0530

    worker tries to publish deleted scheduled post

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -2,13 +2,15 @@ class EmailWorker
   include Sidekiq::Worker 
 
   def perform(user_id, post)
-  	@sender = User.find(user_id)
-  	@users= @sender.user_followers
-  	@post = Post.find_by_id(post)
-    @post.publish!
-  	@blog = Blog.find(@post.blog_id)
-    @users.each do |reciever|
-      SubscriptionMailer.blogpost_email(reciever, @sender, @blog, @post).deliver
-    end 
+    @sender = User.find(user_id)
+    @users= @sender.user_followers
+    @post = Post.find_by_id(post)
+    unless @post.nil? #If Post has not been deleted by the time of publishing.
+      @post.publish!
+      @blog = Blog.find(@post.blog_id)
+      @users.each do |reciever|
+        SubscriptionMailer.blogpost_email(reciever, @sender, @blog, @post).deliver
+      end
+    end
   end
 end
