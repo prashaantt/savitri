3467ee91bd713b5972926e956c97f23f6ac3d4b2
Nishant Modak <modak.nishant@gmail.com>
2013-10-10 12:26:01 +0530

    Improvements for search results download #167

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -88,12 +88,6 @@ class SearchController < ApplicationController
       query = params[:q].split("in:")
       unless query.length > 2
         case query[1].downcase
-          when "books"
-            @search = Sunspot.search Book do
-              fulltext query[0]
-              order_by(:id, :asc)
-              paginate :page => params[:page]
-            end
           when "sentences"
             @search = Sunspot.search Stanza do
               fulltext query[0], :highlight => true
@@ -106,7 +100,6 @@ class SearchController < ApplicationController
               with(:canto).equal_to(params[:canto]) if params[:canto].present?
               with(:length).equal_to(params[:length]) if params[:length].present?
               with(:sbook).equal_to(params[:sbook]) if params[:sbook].present?
-              paginate :page => params[:page]
             end
           when "lines"
             @search = Sunspot.search Line do
@@ -120,45 +113,42 @@ class SearchController < ApplicationController
               with(:canto).equal_to(params[:canto]) if params[:canto].present?
               with(:length).equal_to(params[:length]) if params[:length].present?
               with(:lbook).equal_to(params[:lbook]) if params[:lbook].present?
-              paginate :page => params[:page]
           end
-          when "posts"
-            @search = Sunspot.search Post do
-              fulltext query[0], :highlight => true
-              facet(:posted)
-              facet(:author)
-              facet(:blogname)
-              with(:posted).equal_to(params[:posted]) if params[:posted].present?
-              with(:author).equal_to(params[:author]) if params[:author].present?
-              with(:blogname).equal_to(params[:blogname]) if params[:blogname].present?
-              paginate :page => params[:page]
-            end
         end
       end
     else
-       @search = Sunspot.search Line, Book, Stanza, Post do
+       @search = Sunspot.search Line, Stanza do
         fulltext params[:q], :highlight => true
         facet(:category)
          if params[:category].present?
            with(:category).equal_to(params[:category])
          end
-        paginate :page => params[:page]
       end
     end
-    @file = ''
+    query = params[:q].to_s
+    query << " lbook="+params[:lbook].to_s if params[:lbook].present?
+    query << " sbook=" + params[:sbook].to_s if params[:sbook].present? 
+    query << " canto=" + params[:canto].to_s if params[:canto].present?
+    query << " section=" + params[:section].to_s if params[:section].present?
+    query << " length=" + params[:length].to_s if params[:length].present?
+    query.downcase!
+    @file = 'Results for ['+ query + "](/search?q="+query+")\n\n---------- \n\n"
     @search.each_hit_with_result do |hit,l|
       if l.is_a?(Line)
         anchor_t = l.section.to_s + "." + l.runningno.to_s
-        @file << l.line + "  ||" +"["+anchor_t+"]"+"("+l.share_url+")"+"||\n"
+        @file << l.line + "  ||" +"["+anchor_t+"]"+"("+l.share_url+")"+"||\n\n"
       elsif l.is_a?(Stanza) 
         l.lines.each_with_index do |line, index|
-          @file << line.line + "\n\n"
+          @file << line.line
+          unless index == l.lines.count - 1
+             @file << "\n\n"
+          end
         end
         anchor_t = l.section.to_s + "." + l.runningno.to_s
-        @file << "  ||" +"["+anchor_t+"]"+"("+share_url(l.no)+")"+"||\n"
+        @file << "  ||" +"["+anchor_t+"]"+"("+share_url(l.no)+")"+"||\n\n"
       end
     end
-    send_data @file, :filename => 'results-'+params[:q] + '.txt', :type => 'text/plain'
+    send_data @file, :filename => 'results-'+ query.tr("=","_").tr(" ","_").tr(":","_") + '.txt', :type => 'text/plain'
     
   end
 end
