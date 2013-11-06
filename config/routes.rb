619154637b88c93abc5e4d715af8cfbc5844d421
Nishant Modak <modak.nishant@gmail.com>
2013-11-06 19:43:12 +0530

    Refactor Search #download. Fixes #173
    1. "Filter By" visible only when filters are available.
    2. Remove unused routes for search.
    3. Download MD availble only where available.

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,7 @@ Savitri::Application.routes.draw do
   resources :audios
   resources :media
 
-  get "search/index"
-
-  get "search/results"
-
   get 'search' => 'search#index'
-  get 'search/search' => 'search#search'
-  get 'search/results' => 'search#results'
-  get 'search/download' => 'search#download'
 
   resources :notebooks
 
@@ -29,7 +22,6 @@ Savitri::Application.routes.draw do
   
   get "/profile/:id" => "users#show", :as => :profile
 
-  get '/search' =>  "lines#index"
   resources :blogs
   resources :uploads
   
