615367ead815d78d507581226420b88b5c30b6a3
Nishant Modak <modak.nishant@gmail.com>
2014-01-09 13:21:14 +0530

    Linting fixes for some models.

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,21 +4,21 @@ class Page < ActiveRecord::Base
   validates_length_of :permalink,:url, :minimum => 3
   validates_presence_of :content, :name, :permalink, :md_content
   validates_uniqueness_of  :permalink
-  validates_presence_of :priority, :unless => Proc.new {|m| m.category != "Menu"}
-  validates_uniqueness_of :priority, :unless => Proc.new {|m| m.category != "Menu"}
-  validates :priority, :numericality => {:only_integer => true}, :unless => Proc.new {|m| m.category != "Menu"}
+  validates_presence_of :priority, :unless => Proc.new {|m| m.category != 'Menu'}
+  validates_uniqueness_of :priority, :unless => Proc.new {|m| m.category != 'Menu'}
+  validates :priority, :numericality => {:only_integer => true}, :unless => Proc.new {|m| m.category != 'Menu'}
 
   after_commit :flush_cache
   before_validation :permalink_update
 
-  searchable do 
-    text :content, :stored => true
-    text :name, :stored => true
+  searchable do
+    text :content, stored: :true
+    text :name, stored: :true
 # --facets below--
     string :category
     string :type
     time :published_at do
-     Time.zone.now
+      Time.zone.now
     end
   end
 
@@ -27,7 +27,7 @@ class Page < ActiveRecord::Base
   end
 
   def category
-    if !(topparent = self.myparents[-1]).nil?
+    if !(topparent = myparents[-1]).nil?
       Page.find(topparent).name
     else
       name
@@ -35,50 +35,50 @@ class Page < ActiveRecord::Base
   end
 
   def to_param
-  	"#{permalink}"
+    "#{permalink}"
   end
 
   def self.cached_all
-    Rails.cache.fetch([self.class.name,"cachedall"]) { Page.all.to_a }
+    Rails.cache.fetch([self.class.name, 'cachedall']) { Page.all.to_a }
   end
 
   def self.cached_menu
-    Rails.cache.fetch([self.class.name,"menupages"]) { order(:priority).find_all_by_category("Menu") }
+    Rails.cache.fetch([self.class.name, 'menupages']) { order(:priority).find_all_by_category('Menu') }
   end
 
   def cached_name
-    Rails.cache.fetch([self,"name"]) { name }
+    Rails.cache.fetch([self,'name']) { name }
   end
 
   def flush_cache
-    Rails.cache.delete([self.class.name, "cachedall"])
-    Rails.cache.delete([self.class.name, "menupages"])
-    Rails.cache.delete([self, "name"])
+    Rails.cache.delete([self.class.name, 'cachedall'])
+    Rails.cache.delete([self.class.name, 'menupages'])
+    Rails.cache.delete([self, 'name'])
   end
 
-  def myparents(parents=Array.new)
+  def myparents(parents = [])
     unless self.parent.nil?
       parents << self.parent
       Page.find(self.parent).myparents(parents)
     else
-      if(parents.nil?)
+      if parents.nil?
         return Array.new << self.id
       else
         self.id
       end
     end
-    return parents
+    parents
   end
 
   def self.cached_menu_pages
-    Rails.cache.fetch([name,"menu-pages"]) {order(:priority).find_all_by_category("Menu")}
+    Rails.cache.fetch([name,'menu-pages']) { order(:priority).find_all_by_category('Menu') }
   end
-  
+
   private
 
   def permalink_update
     unless self.parent.nil?
-      newvalue = Page.find(self.parent).permalink.concat("/").concat(self.url)
+      newvalue = Page.find(self.parent).permalink.concat('/').concat(self.url)
       self.permalink = newvalue.downcase
     else
       self.permalink=self.url
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,42 +1,42 @@
 class Section < ActiveRecord::Base
   attr_accessible :name, :no, :references, :canto_id, :runningno, :id
   has_many :stanzas
-  has_many :lines, :through => :stanzas
+  has_many :lines, through: :stanzas
   belongs_to :canto
 
   def cached_stanzas
-  	Rails.cache.fetch([self,"stanzas"]) { stanzas.order('no').to_a }
+    Rails.cache.fetch([self, 'stanzas']) { stanzas.order('no').to_a }
   end
 
   def self.cached_all
-    Rails.cache.fetch([name,"sectionall"]) { order('no').to_a }
+    Rails.cache.fetch([name, 'sectionall']) { order('no').to_a }
   end
 
   def flush_cache
-  	Rails.cache.delete([self.class.name,id])
+    Rails.cache.delete([self.class.name, id])
   end
 
   def cached_runningno
-    Rails.cache.fetch([self, "runningno"]) { runningno }
+    Rails.cache.fetch([self, 'runningno']) { runningno }
   end
-  
+
   def cached_cantoid
-    Rails.cache.fetch([self, "cantoid"]) { canto_id }
+    Rails.cache.fetch([self, 'cantoid']) { canto_id }
   end
 
   def cached_canto
-    Rails.cache.fetch([self, "canto"]) { canto }
+    Rails.cache.fetch([self, 'canto']) { canto }
   end
 
   def cached_number
-    Rails.cache.fetch([self, "no"]) { no }
+    Rails.cache.fetch([self, 'no']) { no }
   end
 
   def self.cached_find_by_no(no)
-    Rails.cache.fetch([name, "findbynumber"+no.to_s]) { find_by_no(no) }
+    Rails.cache.fetch([name, 'findbynumber' + no.to_s]) { find_by_no(no) }
   end
 
   def self.cached_find(id)
-  	Rails.cache.fetch([name, id]) { find(id) }
+    Rails.cache.fetch([name, id]) { find(id) }
   end
 end
