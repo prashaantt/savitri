2157f4464ca9c92a28a04c41dfcf4944912f7c10
Nishant Modak <modak.nishant@gmail.com>
2014-01-30 18:22:03 +0530

    Email notifications only to users who accepted invites. Fixes #196.

--- app/workers/comment_worker.rb
+++ app/workers/comment_worker.rb
@@ -1,16 +1,19 @@
 class CommentWorker
-  include Sidekiq::Worker 
+  include Sidekiq::Worker
 
   def perform(user_id, comment)
-  	@sender = User.find(user_id)
-  	@users= @sender.user_followers
+    @sender = User.find(user_id)
+    @users = @sender.user_followers.select do |u|
+        # if invited user then check if invitation accepted
+        u.invited_by_id.nil? || !u.invitation_accepted_at.nil?
+      end
     @comment = Comment.find_by_id(comment)
-  	@post = Post.find_by_id(@comment.post)
-    @users_followers = Array.new
+    @post = Post.find_by_id(@comment.post)
+    @users_followers = []
     @users.each do |up|
       @users_followers << up.id
     end
-    @early_commenters = Array.new
+    @early_commenters = []
     @post.comments.each do |pc|
       @early_commenters << pc.user.id
     end
@@ -18,6 +21,6 @@ class CommentWorker
     @list_to_send_to = @early_commenters | @users_followers || @post.user.id
     @list_to_send_to.each do |reciever|
       CommentMailer.comment_notification_email(User.find(reciever), @sender, @blog, @post, @comment).deliver!
-    end 
+    end
   end
 end
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,11 +1,14 @@
 class EmailWorker
-  include Sidekiq::Worker 
+  include Sidekiq::Worker
 
   def perform(user_id, post)
     @sender = User.find(user_id)
-    @users= @sender.user_followers
+    @users = @sender.user_followers.select do |u|
+      # if invited user then check if invitation accepted
+      u.invited_by_id.nil? || !u.invitation_accepted_at.nil?
+    end
     @post = Post.find_by_id(post)
-    unless @post.nil? #If Post has not been deleted by the time of publishing.
+    unless @post.nil? # If Post has not been deleted by the time of publishing.
       @post.publish!
       @blog = Blog.find(@post.blog_id)
       @users.each do |reciever|
