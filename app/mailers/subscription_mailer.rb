ff98bacb57b167782c0fd184272132d37024196a
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-02-03 16:04:05 +0530

    Post email author name Fixes #198. Fix of absolute url in blogpost email
    Rubocop : comments_controller, subscription_mailer, User model

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,18 +1,16 @@
+# encoding: UTF-8
+# Send emails to the followers of post author.
+
 class SubscriptionMailer < ActionMailer::Base
-  default :from => "info@savitri.in",
-          :reply_to => "info@savitri.in"
+  default from: 'info@savitri.in', reply_to: 'info@savitri.in'
 
   def blogpost_email(reciever, sender, blog, post)
     @reciever = reciever
     @sender = sender
+    @name = (@sender.name.blank? ? @sender.username : @sender.name)
     @blog = blog
-    @post = post 
-    @url  = "http://savitri.in"
-    if @sender.name.blank?
-      name = @sender.name 
-    else
-      name = @sender.username
-  	end
-    mail(:to => reciever.email, :subject => "#{name} wrote a new post on '#{blog.title}'")
+    @post = post
+    mail(to: reciever.email, subject: "#{ @name } wrote a new post\
+                                                    on '#{blog.title}'")
   end
 end
