5f4af9913fe391f1f7d57abe76ac4acfa9dbc643
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-02-08 19:13:57 +0530

    User follows blog implementation. Rubocop
    Now User can follow blog. So Anyone who follows blog will get all
    notifications if post or comment is created under that blog.
    Also earlier Post author was not getting notification when he creates
    any post. Now that post author will get notification.
    Rubocop : follows_controller, blog.rb, comment_worker, email_worker,
    routes

--- app/workers/comment_worker.rb
+++ app/workers/comment_worker.rb
@@ -1,23 +1,28 @@
+# encoding: UTF-8
+# Send emails to all followers of commenter and blog.
+# and to people who have already commented to the post.
 class CommentWorker
   include Sidekiq::Worker
 
   def perform(user_id, comment)
     @sender = User.find(user_id)
-    @users = @sender.user_followers.select do |u|
-        # if invited user then check if invitation accepted
-        u.invited_by_id.nil? || !u.invitation_accepted_at.nil?
-      end
+    users = @sender.user_followers.select do |u|
+      # if invited user then check if invitation accepted
+      u.invited_by_id.nil? || !u.invitation_accepted_at.nil?
+    end
     @comment = Comment.find_by_id(comment)
     @post = Post.find_by_id(@comment.post)
-    @users_followers = []
-    @users.each do |up|
-      @users_followers << up.id
-    end
-    @early_commenters = @post.comments.pluck(:user_id)
+    early_commenters = @post.comments.map { |com| com.user }
     @blog = Blog.find(@post.blog_id)
-    @list_to_send_to = @early_commenters | @users_followers || @post.author_id
-    @list_to_send_to.each do |reciever|
-      CommentMailer.comment_notification_email(User.find(reciever), @sender, @blog, @post, @comment).deliver!
+    blog_followers = @blog.user_followers.select do |u|
+      # if invited user then check if invitation accepted
+      u.invited_by_id.nil? || !u.invitation_accepted_at.nil?
+    end
+    list_to_send_to = early_commenters | users | blog_followers ||
+                               @post.author_id
+    list_to_send_to.each do |receiver|
+      CommentMailer.comment_notification_email(
+      receiver, @sender, @blog, @post, @comment).deliver!
     end
   end
 end
