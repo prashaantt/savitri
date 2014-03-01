4aa474cf6267fc96e516d8fc35be09ad77af78b5
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-03-01 20:50:48 +0530

    Markdown for comment and excerpt #187 #188. Rubocop
    Rubocop for app/helpers/application_helper.rb, app/mailers/comment_mailer.rb

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,19 +1,23 @@
+# encoding: UTF-8
+# Comment mailer.
 class CommentMailer < ActionMailer::Base
-  default :from => "info@savitri.in",
-          :reply_to => "info@savitri.in"
+  default from: 'info@savitri.in', reply_to: 'info@savitri.in'
+
+  add_template_helper ApplicationHelper
 
   def comment_notification_email(reciever, sender, blog, post, comment)
     @reciever = reciever
     @comment = comment
     @sender = sender
     @blog = blog
-    @post = post 
-    @url  = "http://savitri.in"
+    @post = post
+    @url  = 'http://savitri.in'
     if @sender.name.blank?
-      name = @sender.name 
+      name = @sender.name
     else
       name = @sender.username
-  	end
-    mail(:to => reciever.email, :subject => "#{name} wrote a comment on '#{blog.title}'")
+    end
+    mail(to: reciever.email, subject: "#{name} wrote a comment on\
+         '#{blog.title}'")
   end
 end
