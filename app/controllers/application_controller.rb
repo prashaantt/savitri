f55769236c6e1dc03714c7c5e2072b9264454109
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-04-02 14:46:15 +0530

    After logging from Blog#index or #show, it will now redirects to same page, And not on recentcomments Fixes #215
    Rubocop applicationcontroller and blogscontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
+# encoding: UTF-8
+# ApplicationController
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = "Access denied!"
+    flash[:error] = 'Access denied!'
     redirect_to savitri_path
   end
 
   def store_location
-      session[:resource_return_to] = request.fullpath
+    session[:resource_return_to] = request.fullpath
   end
 
   def last_page
@@ -19,10 +21,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    savitri_path 
-  end 
+    savitri_path
+  end
 
   def not_found
-    raise ActionController::RoutingError.new('Couldn\'t find the page you were looking for.')
+    fail ActionController::RoutingError.new('Couldn\'t find the page you were'\
+        ' looking for.')
   end
 end
