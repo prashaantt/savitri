3d9b70c9afeecb1fcb71e0a253f65ae81f7a84b7
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-05-15 16:50:02 +0530

    Handled responses when user does not exist.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,11 @@
 class ErrorsController < ApplicationController
-	
-	def show
-		@exception = env["action_dispatch.exception"]
-		respond_to do |format|
-			format.html { render action: request.path[1..-1], status: request.path[1..-1] }
-			format.json { render json: {status: request.path[1..-1], error: @exception.message}, status: request.path[1..-1] }
-		end
-	end
+
+  def show
+    @exception = env["action_dispatch.exception"]
+    respond_to do |format|
+      format.html { render action: request.path[1..-1], status: request.path[1..-1] }
+      format.json { render json: {status: request.path[1..-1], error: @exception.message}, status: request.path[1..-1] }
+      format.atom { render action: 'atom', status: request.path[1..-1] }
+    end
+  end
 end
