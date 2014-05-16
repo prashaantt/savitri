1974fb68d1304bc76c040bc2a696c8f9b8a32726
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-05-16 18:24:54 +0530

    Set default status code 200 for json and atom in errors controller.
    UsersController Indented.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,8 +4,8 @@ class ErrorsController < ApplicationController
     @exception = env["action_dispatch.exception"]
     respond_to do |format|
       format.html { render action: request.path[1..-1], status: request.path[1..-1] }
-      format.json { render json: {status: request.path[1..-1], error: @exception.message}, status: request.path[1..-1] }
-      format.atom { render action: 'atom', status: request.path[1..-1] }
+      format.json { render json: {status: request.path[1..-1], error: @exception.message} }
+      format.atom { render layout: false, content_type: 'application/xml', action: 'atom' }
     end
   end
 end
--- app/views/errors/atom.atom.builder
+++ app/views/errors/atom.atom.builder
@@ -1,4 +1,4 @@
-atom_feed do |feed|
+atom_feed :language => 'en-US', :id => @feed_url do |feed|
   title = request.path[1..-1] == '404'? ' Not Found' : ' Internal Server Error'
   feed.title(request.path[1..-1],title)
   feed.content(@exception.message)
