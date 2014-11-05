7c14c5fb6a97bad5c87502c26209fdb78aa30d79
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-11-05 18:08:57 +0530

    Featured Stanzas for landing page

--- app/controllers/savitri_controller.rb
+++ app/controllers/savitri_controller.rb
@@ -2,8 +2,8 @@
 # SavitriController is Home Page
 class SavitriController < ApplicationController
   def index
-    @sentences = Stanza.find(:all).sample(10)
-    @selections = @sentences.collect{|c| c.cached_lines.map(&:line).join('<br/>').html_safe}
+    @selections = Stanza.where(featured:true).limit(10)
+    #@selections = @sentences.collect{|c| c.cached_lines.map(&:line).join('<br/>').html_safe}
   end
 
   def show
