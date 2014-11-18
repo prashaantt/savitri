4dac773a755e4272aa75a557c01155c0149fc699
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-11-18 14:46:40 +0530

    Recent posts on home page

--- app/controllers/savitri_controller.rb
+++ app/controllers/savitri_controller.rb
@@ -3,6 +3,9 @@
 class SavitriController < ApplicationController
   def index
     @selections = Stanza.where(featured:true).sample(10)
+    @posts_1 = @blog_1.posts.where(featured: false).published.order('published_at DESC').limit(5)
+    @posts_2 = @blog_2.posts.where(featured: false).published.order('published_at DESC').limit(5)
+    @posts_3 = @blog_3.posts.where(featured: false).published.order('published_at DESC').limit(5)
   end
 
   def show
