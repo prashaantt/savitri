d0988cebf02d11c8fb0f3746fd70c1900a762f23
Sagar Junnarkar <sagarjunnu@gmail.com>
2015-02-02 14:18:25 +0530

    Fixes #296
    Unique cache key

--- app/controllers/read_controller.rb
+++ app/controllers/read_controller.rb
@@ -43,7 +43,7 @@ class ReadController < ApplicationController
   def specific
     edition = find_edition_by_year params
     query = params[:book_id].split('.')
-    section = edition.sections.cached_find_by_no(query[0])
+    section = edition.sections.cached_find_by_no(query[0],edition.year)
     sectionrunningno = section.cached_runningno
     canto=section.canto
     cantoid = canto.cached_number
